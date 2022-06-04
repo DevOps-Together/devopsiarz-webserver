56eca4edb830407455cc1049df458e9f1679c81e
Krzysztof Prorok <Krzysztof1397@gmail.com>
2022-06-04 22:55:34 +0200

    Turns out there is no need to switch port

--- server/server.py
+++ server/server.py
@@ -9,7 +9,7 @@ class Server:
     
     def start():
         # TODO: loop listening on port from configuration, starting a new thread for each incoming connection (use some kind of thread pool with limit on maximal number of threads)
-        # TODO: threads that establish connection on another port, retrive and return resource or appropriate error
+        # TODO: threads that establish connection, retrive and return resource or appropriate error
         # TODO: handle http basic auth for incoming connections
         # TODO: cleanup of all threads and resources on exit
         return
