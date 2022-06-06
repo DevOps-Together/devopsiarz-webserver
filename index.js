28f679549ea0ab96fd83975c9dfd8783f0f53e50
= <=>
2022-06-06 18:36:47 +0200

    first server

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
