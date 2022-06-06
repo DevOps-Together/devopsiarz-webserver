18206bb8ae24bb35ced1f676b7d84537e08839ba
= <=>
2022-06-06 19:43:38 +0200

    www root index file or 404 error if file not exist (index only)

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+
+const wwwRoot = __dirname + '/www/';
+console.log(wwwRoot);
 
 const host = 'localhost';
 const port = 8000;
@@ -12,11 +16,22 @@ const requestListener = function (req, res) {
     // map request url to filesystem:
     if (url_parts.pathname === '/' || url_parts.pathname === '/index.html') {
         console.log('www root index');
+        fs.readFile(wwwRoot + 'index.html', 'UTF-8', (err, data) => {
+            if (err) {
+                return error404(res);
+            }
+            res.writeHead(200);
+            res.end(data);
+        })
     }
 
-    res.writeHead(200);
-    res.end(`url: ${JSON.stringify(url_parts)} `);
+    //  res.end(`url: ${JSON.stringify(url_parts)} `);
 };
+
+const error404 = (res) => {
+    res.writeHead(404);
+    res.end("File not found");
+}
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
