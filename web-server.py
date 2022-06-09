99e122d44ea88555b0ddb6c8190623784ef4b742
sew <38424208+iamdsx@users.noreply.github.com>
2022-06-09 21:21:09 +0200

    new version

--- web-server.py
+++ web-server.py
@@ -1,27 +1,43 @@
-import http
-from http.server import HTTPServer
-import time
+# Import server module
 
-hostName = "localhost"
-serverPort = 8080
+import http.server
 
-class MyServer(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        if self.path == '/':
-            self.path = 'index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+# Import SocketServer module
 
-if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Serwer: http://%s:%s" % (hostName, serverPort))
+import socketserver
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
+# Import sys module
 
-    webServer.server_close()
-    print("Serwer zatrzymany.")
+import sys
+
+
+try:
+
+    # Set the port number
+
+    port = 8080
+
+    # Set the IP address
+
+    server_address = ('localhost', port)
+
+    # Create object for handling HTTP requests
+
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    # Run the web server forever to handle the HTTP requests
+
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+
+        print("Web Server is running at http://localhost:%s" %port)
+
+        httpd.serve_forever()
+
+# Stopped the server
+
+except KeyboardInterrupt:
+    pass
+
+httpd.server_close()
+
+print("The server is stopped.")
